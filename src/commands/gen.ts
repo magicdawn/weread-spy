ef07af50840cdc5b1258dd45b38916253e958fe1
magicdawn <magicdawn@qq.com>
2022-02-13 01:44:30 +0800

    feat: support 'gen --id xxx'

--- src/commands/gen.ts
+++ src/commands/gen.ts
@@ -16,15 +16,19 @@ export default class extends Command {
       'book start url. e.g(https://weread.qq.com/web/reader/41432f705de453414ca0b4akc81322c012c81e728d9d180)',
   })
 
+  id: string = Option.String('-i,--id', {
+    description: 'book id. e.g(812443)',
+  })
+
   clean: boolean = Option.Boolean('-c,--clean')
 
   async execute() {
-    const {url, clean} = this
-    main({url, clean})
+    const {url, clean, id} = this
+    main({url, clean, id})
   }
 }
 
-export async function main({url, clean}: {url: string; clean: boolean}) {
+export async function main({url, clean, id}: {url?: string; clean: boolean; id?: string}) {
   let map: unknown
   try {
     map = await fse.readJsonSync(path.join(APP_ROOT, 'data/book/map.json'))
@@ -32,11 +36,19 @@ export async function main({url, clean}: {url: string; clean: boolean}) {
     map = {}
   }
 
-  const {bookId: id} = map[url] || {}
-  if (!id) {
-    return console.error('can not find id !!!')
+  let bookId = ''
+
+  if (url) {
+    ;({bookId} = map[url] || {})
+    if (!bookId) {
+      return console.error('can not find id !!!')
+    }
   }
 
-  await genEpubFor(id, clean)
-  await checkEpub(id)
+  if (id) {
+    bookId = id
+  }
+
+  await genEpubFor(bookId, clean)
+  await checkEpub(bookId)
 }
