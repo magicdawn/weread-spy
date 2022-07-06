b9c6423e192189b2cb4a5f65d79b019aa9feebf1
magicdawn <magicdawn@qq.com>
2022-07-06 16:12:27 +0800

    chore: fix worker comment

--- src/utils/processContent/index.main.ts
+++ src/utils/processContent/index.main.ts
@@ -8,10 +8,12 @@ type ProcessContent = typeof processContent
 
 /**
  * 如果使用 ts-node
- * 开发使用 __dirname + /processContent.worker.js
- * 内容是 require ts-node/register + require processContent.worker.ts
+ * 开发使用 __dirname + /index.worker.js
+ * 内容是 require ts-node/register + require index.worker.ts
  *
- * ts 编译后, processContent.worker.ts -> processContent.worker.js
+ * ts 编译后,
+ * ./index.worker.ts -> dist/index.worker.js
+ * ./index.worker.js 不参与 ts 编译
  */
 
 export function createWorker() {
