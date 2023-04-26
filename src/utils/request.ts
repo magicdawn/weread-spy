ef61553706462a45316276bf20055f530615c52f
Tao <magicdawn@qq.com>
2023-04-26 20:30:03 +0800

    chore: rm more deps

--- src/utils/request.ts
+++
@@ -1,8 +0,0 @@
-import { extend } from 'umi-request'
-
-export default extend({
-  headers: {
-    'user-agent':
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
-  },
-})
